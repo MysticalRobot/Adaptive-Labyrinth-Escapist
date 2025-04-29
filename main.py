6dfebf4ac07afbc47d27775b4916d78463ef2173
MysticalRobot <dhruv21420@gmail.com>
2025-04-29 19:16:00 -0400

    replaced current_maze.txt with previous_maze.txt as the file generated by main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from init_algorithm import initialize_algorithm
 # initialize graph and algorithm 
 maze_to_recreate = '' # replace with 'previous_maze.txt' to use the last maze
 _, algorithm = initialize_algorithm(algorithm_name='dstar', n=10, allow_diagonal_movement=True, maze_to_recreate=maze_to_recreate)
-with open('current_maze.txt', 'w') as f:
+with open('previous_maze.txt', 'w') as f:
     f.write(algorithm.G.GetRecreationInfo())
 algorithm.ComputeShortestPath() 
 print(algorithm.G)
