4f58f0382248a70d21d7819209890adf7b73279b
MysticalRobot <dhruv21420@gmail.com>
2025-05-02 12:00:51 -0400

    automated plot generation and updated readme accordingly

--- collect_data.py
+++ collect_data.py
@@ -7,6 +7,12 @@ from algorithms import algorithms
 # TODO add/remove algorithm names from this list
 algorithm_names = list(algorithms.keys())
 
+# define range of n to gather data about
+n_lo, n_hi, n_step = 2, 53, 5
+
+# repeat each measurement this many times:
+num_trials = 5
+
 # use the same seed for all the algorithms to compare them on the exact same mazes
 random_seed = 69
 random.seed(random_seed)
@@ -57,12 +63,6 @@ def measure_performance(algorithm_name : str, n : int, allow_diagonal_movement :
     
     return (execution_time, path_length)
 
-# define range of n to gather data about
-n_lo, n_hi, n_step = 2, 13, 5
-
-# repeat each measurement this many times:
-num_trials = 5
-
 # generate rows of data
 data = [[str(n)] for n in range(n_lo, n_hi, n_step)]
 # for each algorithm and movement setting:
@@ -77,7 +77,7 @@ for algorithm_name in algorithm_names:
                 execution_time, path_length = measure_performance(algorithm_name=algorithm_name, n=n, allow_diagonal_movement=allow_diagonal_movement)
                 execution_times += execution_time
                 path_lengths += path_length # this number is probably constant across trials but whatever
-            data[i].append(str(execution_times // num_trials))
+            data[i].append(str((execution_times // num_trials) * 10**(-9)))
             data[i].append(str(path_lengths // num_trials))
             i += 1
 
