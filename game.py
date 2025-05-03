0a504903c1304066a8d9454a57d408965d0f1cfd
MysticalRobot <dhruv21420@gmail.com>
2025-05-02 23:25:29 -0400

    added header to GUI and gif to readme

--- game.py
+++ game.py
@@ -8,7 +8,10 @@ from algorithms import algorithms
 algorithm_name = 'd* lite'
 
 # generate maze and initialize algorithm
-G = Graph(n=10, allow_diagonal_movement=False)
+maze_to_recreate = 'previous_maze.txt' # replace with 'previous_maze.txt' to use the last maze
+G = Graph(n=10, allow_diagonal_movement=True, maze_to_recreate=maze_to_recreate)
+with open('previous_maze.txt', 'w') as f:
+    f.write(G.GetRecreationInfo())
 algorithm = algorithms[algorithm_name](G)
 algorithm.ComputePath() 
 
@@ -33,13 +36,22 @@ venting_sound.set_volume(4)
 
 # initialize font
 pygame.font.init()
-# print(pygame.font.get_fonts())
-font = pygame.font.SysFont(name='helvetica', size=vertex_size // 4)
+vertex_label_font = pygame.font.SysFont(name='helvetica', size=vertex_size // 4)
+algorithm_name_font = pygame.font.SysFont(name='helvetica', size=20) 
+header_detail_font = pygame.font.SysFont(name='helvetica', size=15) 
 
 # initialize pygame
 pygame.init()
-dimension = (G.old_n * vertex_size + (G.old_n - 1) * edge_size)
-screen = pygame.display.set_mode((dimension, dimension))
+maze_dimension = (G.old_n * vertex_size + (G.old_n - 1) * edge_size)
+header_height = 110
+# create the screen, which is comprised of a header and the maze
+screen = pygame.display.set_mode((maze_dimension + 2 * edge_size, maze_dimension + 3 * edge_size + header_height)) 
+header = pygame.surface.Surface((screen.get_width(), header_height))
+header_x_offset = edge_size
+header_y_offset = edge_size
+maze = pygame.surface.Surface((maze_dimension, maze_dimension))
+maze_x_offset = edge_size
+maze_y_offset = edge_size * 2 + header_height
 clock = pygame.time.Clock()
 facing_right = True
 time_since_end_of_game = None
@@ -58,15 +70,17 @@ def get_edge_color(edge : int) -> str:
     else:
         return 'grey'
 
-# returns the edge located at pos on the screen
+# returns the edge located at pos on the maze
 def get_edge_at_pos(pos : Tuple[int, int]) -> Tuple[int, int]:
+    # subtract the maze's offset from the postion on the display
+    pos_x, pos_y = pos[0] - maze_x_offset, pos[1] - maze_y_offset
     y_offset = 0
     for y in range(G.n):
         height = edge_size if G.IsHorizontalWall(y) else vertex_size
         x_offset = 0
         for x in range(G.n):
             width = edge_size if G.IsVerticalWall(x) else vertex_size
-            if pos[1] > y_offset and pos[1] < (y_offset + height) and pos[0] > x_offset and pos[0] < (x_offset + width):
+            if pos_y > y_offset and pos_y < (y_offset + height) and pos_x > x_offset and pos_x < (x_offset + width):
                 if G.IsEdge((y, x)) and not (G.IsDiagonalEdge((y, x)) and not G.allow_diagonal_movement):
                     return (y, x)
                 else:
@@ -78,7 +92,7 @@ def get_edge_at_pos(pos : Tuple[int, int]) -> Tuple[int, int]:
 
 # draws the maze
 def draw_maze() -> None:
-    screen.fill('grey') # draw the background
+    maze.fill('grey') # draw the background
     y_offset = 0
     for y in range(G.n):
         height = edge_size if G.IsHorizontalWall(y) else vertex_size
@@ -90,33 +104,65 @@ def draw_maze() -> None:
                 if time_since_end_of_game is None:
                     # draw the goal (vent) before the start (amongus) to account for overlap
                     if (y, x) == G.goal:
-                        screen.blit(vent, (x_offset + vertex_size * 0.1, y_offset + vertex_size * 0.5))
+                        maze.blit(vent, (x_offset + vertex_size * 0.1, y_offset + vertex_size * 0.5))
                     elif (y, x) == G.start:
-                        screen.blit(amongus, (x_offset + vertex_size * 0.1, y_offset + vertex_size * 0.025))
+                        maze.blit(amongus, (x_offset + vertex_size * 0.1, y_offset + vertex_size * 0.025))
                 # draw the venting animation based on time elapsed since end of game
                 else:
                     current_time = time.time_ns()
                     if current_time < time_since_end_of_game + frame_time_length * 5:
-                        screen.blit(venting_frames[0], (x_offset, y_offset))
+                        maze.blit(venting_frames[0], (x_offset, y_offset))
                     # second frame occurs three times in original gif
                     elif current_time < time_since_end_of_game + frame_time_length * 8:
-                        screen.blit(venting_frames[1], (x_offset, y_offset))
+                        maze.blit(venting_frames[1], (x_offset, y_offset))
                     elif current_time < time_since_end_of_game + frame_time_length * 9:
-                        screen.blit(venting_frames[2], (x_offset, y_offset))
+                        maze.blit(venting_frames[2], (x_offset, y_offset))
                     elif current_time < time_since_end_of_game + frame_time_length * 10:
-                        screen.blit(venting_frames[3], (x_offset, y_offset))
+                        maze.blit(venting_frames[3], (x_offset, y_offset))
                     else:
-                        screen.blit(venting_frames[4], (x_offset, y_offset))
+                        maze.blit(venting_frames[4], (x_offset, y_offset))
             # draw og walls black, newly added walls red, and removed walls green
             elif curr != G.EMPTY_OR_EDGE:
-                pygame.draw.rect(screen, get_edge_color(curr), pygame.Rect(x_offset, y_offset, width, height))
+                pygame.draw.rect(maze, get_edge_color(curr), pygame.Rect(x_offset, y_offset, width, height))
             # draw rhs and g values in the center of vertices for d* lite
             elif not G.IsEdge((y, x)) and algorithm_name == 'd* lite':
-                text = font.render(f'{algorithm.rhs[(y, x)]}:{algorithm.g[(y, x)]}', True, 'black')
-                screen.blit(text, (x_offset + (vertex_size - text.get_width()) // 2, y_offset + (vertex_size - font.get_height()) // 2))
+                vertex_data = vertex_label_font.render(f'{algorithm.rhs[(y, x)]}:{algorithm.g[(y, x)]}', True, 'black')
+                maze.blit(vertex_data, (x_offset + (vertex_size - vertex_data.get_width()) // 2, y_offset + (vertex_size - vertex_label_font.get_height()) // 2))
             x_offset += width
         y_offset += height
-    pygame.display.update() # update the screen
+    screen.blit(maze, (maze_x_offset, maze_y_offset))
+    
+def draw_screen() -> None:
+    # draw the maze
+    draw_maze()
+    
+    # draw the each line of the header separately
+    header.fill('black')
+    text_x_offset = edge_size
+    text_y_offset = edge_size
+    line_spacing = 10
+    
+    text = algorithm_name_font.render(f'algorithm: {algorithm_name}', True, 'white')
+    header.blit(text, (text_x_offset, text_y_offset))
+    text_y_offset += algorithm_name_font.get_height() + line_spacing
+
+    text = header_detail_font.render("controls: press 'a'/'d' to randomly add/delete edges, 'w' to move towards the goal,", True, 'white')
+    header.blit(text, (text_x_offset, text_y_offset))
+    text_y_offset += header_detail_font.get_height() + line_spacing
+
+    text = header_detail_font.render('and right click on edges to manually add or remove them', True, 'white')
+    header.blit(text, (text_x_offset + header_detail_font.render('controls: ', True, 'white').get_width(), text_y_offset))
+    text_y_offset += header_detail_font.get_height() + line_spacing
+
+    # TODO add notes about algorithm visualizations here
+    if algorithm_name == 'd* lite':
+        text = header_detail_font.render('note: every vertex s is labeled with its rhs and g values in the form rhs(s):g(s)', True, 'white')
+        header.blit(text, (text_x_offset, text_y_offset))
+
+    screen.blit(header, (header_x_offset, header_y_offset))
+
+    # update the screen
+    pygame.display.update() 
 
 while running:
     for event in pygame.event.get():
@@ -168,7 +214,7 @@ while running:
                     # flip all the venting frames based on the final orientation
                     if not facing_right:
                         venting_frames = [pygame.transform.flip(frame, True, False) for frame in venting_frames]
-    draw_maze()
+    draw_screen()
     clock.tick(60) # cap at 60 fps
 
 pygame.quit()
